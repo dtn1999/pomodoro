4f7713635e75fabac60b6f60431461486a761065
dtn1999 <tchekamboudanyls@gmail.com>
2021-10-13 13:51:13 +0200

    update theme.colors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,14 @@ const theme = extendTheme({
   shadows: {
     timer: "-88px -36px 105px 52px rgba(40,48,89,0.35)",
   },
+  colors: {
+    bgColor: "#1F2440",
+    primary: "#F26D6D",
+    inputBgColor: "",
+    text: "#D0D9F2",
+    dark: "#111426",
+    darkText: "#737896",
+  },
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
