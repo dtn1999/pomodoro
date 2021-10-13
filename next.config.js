d9d0daeda10c38783d572f4fd43a9536ed906dee
dtn1999 <tchekamboudanyls@gmail.com>
2021-10-13 02:46:37 +0200

    update next.config.js to support

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+const compose = require("next-compose");
+
+module.exports = compose([
+  {
+    reactStrictMode: true,
+  },
+  {
+    webpack(config, options) {
+      config.module.rules.push({
+        test: /\.mp3$/,
+        use: {
+          loader: "file-loader",
+        },
+      });
+      return config;
+    },
+  },
+]);
